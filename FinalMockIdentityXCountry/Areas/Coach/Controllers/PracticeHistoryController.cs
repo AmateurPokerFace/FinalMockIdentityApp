5e6c94162ed8273ca78f9877d5857e3a19263597
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-10-28 02:54:56 -0400

    Added a page for practice history's DateTime, location, and total attending runners.

--- FinalMockIdentityXCountry/Areas/Coach/Controllers/PracticeHistoryController.cs
+++ FinalMockIdentityXCountry/Areas/Coach/Controllers/PracticeHistoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Build.Evaluation.Context;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using NuGet.Versioning;
 using System.Data;
 using System.Security.Claims;
@@ -25,10 +26,57 @@ namespace FinalMockIdentityXCountry.Areas.Coach.Controllers
             _userManager = userManager;
         }
 
+        public IActionResult Index() 
+        {
+            return View(); 
+        }
+
         public IActionResult History()
         {
+            List<HistoryViewModel> historyViewModels = new List<HistoryViewModel>();
+ 
 
-            return View();
+            var dbQueries = (from p in _context.Practices
+                                  join a in _context.Attendances
+                                  on p.Id equals a.PracticeId
+                                  where a.IsPresent && p.PracticeIsInProgress == false
+                                  group a by new
+                                  {
+                                      a.PracticeId,
+                                      p.PracticeLocation,
+                                      p.PracticeStartTimeAndDate
+                                  } into matchesFound
+                                  select new HistoryViewModel()
+                                  {
+                                      PracticeId = matchesFound.Key.PracticeId,
+                                      PracticeDateTime = matchesFound.Key.PracticeStartTimeAndDate,
+                                      PracticeLocation = matchesFound.Key.PracticeLocation,
+                                      TotalRunners = matchesFound.Count(),
+                                  });
+
+            if (dbQueries.Count() > 0)
+            {
+                foreach (var dbQuery in dbQueries)
+                {
+                    HistoryViewModel historyViewModel = new HistoryViewModel
+                    {
+                        PracticeId = dbQuery.PracticeId,
+                        PracticeDateTime = dbQuery.PracticeDateTime,
+                        PracticeLocation = dbQuery.PracticeLocation,
+                        TotalRunners = dbQuery.TotalRunners
+                    };
+
+                    historyViewModels.Add(historyViewModel);
+                } 
+            }
+
+            if (historyViewModels.Count() > 0)
+            {
+                historyViewModels = historyViewModels.OrderByDescending(d => d.PracticeDateTime).ToList();
+                return View(historyViewModels);
+            }
+
+            return RedirectToAction(nameof(Index)); // send to an error page in the future (no practice history found)
         }
     }
 }
