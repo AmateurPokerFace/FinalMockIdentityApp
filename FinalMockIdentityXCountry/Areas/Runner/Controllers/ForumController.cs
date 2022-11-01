be73500df677d61a814f9e0e7e404f706a0fdfa5
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-11-01 17:44:41 -0400

    Added ForumController (runner folder)

---
+++ FinalMockIdentityXCountry/Areas/Runner/Controllers/ForumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalMockIdentityXCountry.Areas.Runner.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
