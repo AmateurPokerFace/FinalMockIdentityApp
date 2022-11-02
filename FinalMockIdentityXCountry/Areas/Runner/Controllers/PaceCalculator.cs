e20168621ae84e96a85b3dacca845060d1a5a39b
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-11-02 02:16:13 -0400

    Added Jack Daniels VDOT calculator page and Omni Calculator page

---
+++ FinalMockIdentityXCountry/Areas/Runner/Controllers/PaceCalculator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace FinalMockIdentityXCountry.Areas.Runner.Controllers
+{
+    [Authorize(Roles = "Master Admin, Coach, Runner")]
+    [Area("Runner")]
+    public class PaceCalculator : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SelectCalculator()
+        {
+            return View();
+        }
+
+        public IActionResult JackDanielsVDOTCalculator()
+        {
+            return View();
+        }
+
+        public IActionResult OmniCalculator()
+        {
+            return View();
+        }
+    }
+}
