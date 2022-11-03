b374bcb51d6ed5449c26c31b9c54cd1ecd9028a2
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-11-03 00:37:09 -0400

    Added a way to end the current practice (Coach view).

--- FinalMockIdentityXCountry/Areas/Coach/Controllers/EndCurrentPracticeController.cs
+++ FinalMockIdentityXCountry/Areas/Coach/Controllers/EndCurrentPracticeController.cs
@@ -24,26 +24,44 @@ namespace FinalMockIdentityXCountry.Areas.Coach.Controllers
             _userManager = userManager;
         }
 
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
         public IActionResult End(int currentPracticeId)
         {
             Practice? practice = _context.Practices.Where(p => p.PracticeIsInProgress).Where(p => p.Id == currentPracticeId).FirstOrDefault();
             
             if (practice != null)
             {
-                practice.PracticeIsInProgress = false;
                 return View(practice);
             }
 
-            return View();  // return a view -> No practices match the id that was provided
+            return RedirectToAction();  // send to an error page in the future. return a view -> No practices match the id that was provided
         }
 
         [HttpPost]
-        public IActionResult End(Practice practice)
+        public IActionResult End(int id, string dummyString)
         {
+            if (id == 0)
+            {
+                return RedirectToAction();  // send to an error page in the future.
+            }
 
+            Practice practice = _context.Practices.Find(id);
 
+            if (practice == null)
+            {
+                return RedirectToAction();  // send to an error page in the future.
+            }
 
-            return View();
+            practice.PracticeEndTimeAndDate = DateTime.Now;
+            practice.PracticeIsInProgress = false;
+            _context.Practices.Update(practice);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index)); // send to a page showing each runner who hasn't signed out.
         }
     }
 }
