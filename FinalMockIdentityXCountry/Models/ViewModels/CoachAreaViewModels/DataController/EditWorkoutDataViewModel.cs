eb7950298a1942f2f51980299a7d234174fc4da1
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-11-05 01:23:09 -0400

    Added Data Controller (Coach View)

---
+++ FinalMockIdentityXCountry/Models/ViewModels/CoachAreaViewModels/DataController/EditWorkoutDataViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FinalMockIdentityXCountry.Models.ViewModels.CoachAreaViewModels.DataController
+{
+    public class EditWorkoutDataViewModel
+    {
+        public EditWorkoutDataViewModel()
+        {
+            Workouts = new List<string>();
+        }
+
+        public string? RunnerName { get; set; }
+        public string? RunnerId { get; set; }
+        public int WorkoutInformationId { get; set; }
+        public int PracticeId { get; set; }
+        public DateTime PracticeStartTime { get; set; }
+        public string? PracticeLocation { get; set; }
+        public List<string>? Workouts { get; set; }
+    }
+}
