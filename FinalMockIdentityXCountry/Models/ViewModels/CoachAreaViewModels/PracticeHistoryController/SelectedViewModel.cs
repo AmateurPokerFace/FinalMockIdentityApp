e709bdcdf8e769b9fc226cd282853ca21772caa1
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-11-28 12:59:27 -0500

    Updated PracticeHistory/Selected

---
+++ FinalMockIdentityXCountry/Models/ViewModels/CoachAreaViewModels/PracticeHistoryController/SelectedViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalMockIdentityXCountry.Models.ViewModels.CoachAreaViewModels.PracticeHistoryController
+{
+    public class SelectedViewModel
+    {
+        public SelectedViewModel()
+        {
+            SelectedViewModelHelpers = new List<SelectedViewModelHelper>();
+        }
+        public string? PracticeLocation { get; set; }
+        public TimeOnly PracticeStartTime { get; set; }
+        public TimeOnly PracticeEndTime { get; set; }
+        public List<SelectedViewModelHelper>? SelectedViewModelHelpers { get; set; }
+    }
+}
---
+++ FinalMockIdentityXCountry/Models/ViewModels/CoachAreaViewModels/PracticeHistoryController/SelectedViewModelHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalMockIdentityXCountry.Models.ViewModels.CoachAreaViewModels.PracticeHistoryController
+{
+    public class SelectedViewModelHelper
+    {
+        public SelectedViewModelHelper()
+        {
+            Workouts = new List<string>();
+        }
+
+        public string? RunnerName { get; set; }
+        public string? RunnerId { get; set; }
+        public List<string>? Workouts { get; set; }
+    }
+}
