3552df09704d1b13ddc492359a10277f7cc5ee9b
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-11-28 20:56:45 -0500

    Updated model classes and fixed a bug in the CurrentPractices action method

---
+++ FinalMockIdentityXCountry/Models/ViewModels/CoachAreaViewModels/CurrentPracticeController/AddRunnerToCurrentPracticeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace FinalMockIdentityXCountry.Models.ViewModels.CoachAreaViewModels.CurrentPracticeController
+{
+    public class AddRunnerToCurrentPracticeViewModel
+    {
+        public AddRunnerToCurrentPracticeViewModel()
+        {
+            SelectedCheckboxOptions = new List<AddRunnerToCurrentPracticeChkboxOptionViewModel>();
+        }
+
+        [ValidateNever]
+        public string? Name { get; set; }
+        [ValidateNever]
+        public string? RunnerId { get; set; }
+        public int AttendanceId { get; set; }
+        public string? PracticeLocation{ get; set; }
+        public DateTime PracticeStartTimeAndDate { get; set; }
+        public List<AddRunnerToCurrentPracticeChkboxOptionViewModel>? SelectedCheckboxOptions { get; set; }
+    }
+}
