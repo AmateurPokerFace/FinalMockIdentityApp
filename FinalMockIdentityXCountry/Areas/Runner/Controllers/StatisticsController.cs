62024397dacabbdfbd48c361f196c82d4a1a5a9a
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-12-05 22:39:10 -0500

    Latest fix

--- FinalMockIdentityXCountry/Areas/Runner/Controllers/StatisticsController.cs
+++ FinalMockIdentityXCountry/Areas/Runner/Controllers/StatisticsController.cs
@@ -93,15 +93,15 @@ namespace FinalMockIdentityXCountry.Areas.Runner.Controllers
 
                 if (selectRangeForStatisticsViewModel.SelectedQueryFilter.ToLower() == "one week")
                 {
-                    dateFromSelectedFilter = selectRangeForStatisticsViewModel.InitialDate.Value.AddDays(7);
+                    dateFromSelectedFilter = selectRangeForStatisticsViewModel.InitialDate.AddDays(7);
                 }
                 else if (selectRangeForStatisticsViewModel.SelectedQueryFilter.ToLower() == "two weeks")
                 {
-                    dateFromSelectedFilter = selectRangeForStatisticsViewModel.InitialDate.Value.AddDays(14);
+                    dateFromSelectedFilter = selectRangeForStatisticsViewModel.InitialDate.AddDays(14);
                 }
                 else if (selectRangeForStatisticsViewModel.SelectedQueryFilter.ToLower() == "one month")
                 {
-                    dateFromSelectedFilter = selectRangeForStatisticsViewModel.InitialDate.Value.AddMonths(1);
+                    dateFromSelectedFilter = selectRangeForStatisticsViewModel.InitialDate.AddMonths(1);
                 }
                 else
                 {
@@ -116,7 +116,7 @@ namespace FinalMockIdentityXCountry.Areas.Runner.Controllers
                                      on wi.PracticeId equals p.Id
                                      join wt in _context.WorkoutTypes
                                      on wi.WorkoutTypeId equals wt.Id
-                                     where wi.RunnerId == userClaimValue && DateOnly.FromDateTime(p.PracticeStartTimeAndDate) >= DateOnly.FromDateTime(selectRangeForStatisticsViewModel.InitialDate.Value.Date)
+                                     where wi.RunnerId == userClaimValue && DateOnly.FromDateTime(p.PracticeStartTimeAndDate) >= DateOnly.FromDateTime(selectRangeForStatisticsViewModel.InitialDate.Date)
                                      && DateOnly.FromDateTime(p.PracticeStartTimeAndDate) <= DateOnly.FromDateTime(dateFromSelectedFilter.Date)
                                      select new
                                      {
