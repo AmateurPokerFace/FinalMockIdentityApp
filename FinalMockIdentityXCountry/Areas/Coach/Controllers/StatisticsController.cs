ed3693fb2d7ca44f4ed5fd0729b66c48f12b2cb8
Ricardo <Ricardo@DESKTOP-OGV37PG>
2022-12-08 10:16:52 -0500

    Fixed navbar links

--- FinalMockIdentityXCountry/Areas/Coach/Controllers/StatisticsController.cs
+++ FinalMockIdentityXCountry/Areas/Coach/Controllers/StatisticsController.cs
@@ -192,60 +192,6 @@ namespace FinalMockIdentityXCountry.Areas.Coach.Controllers
             return RedirectToAction(nameof(Index));
 
         }
-        public IActionResult SelectedRunner(string runnerId)
-        {
-            if (runnerId == null)
-            {
-                return RedirectToAction("Index");
-            }
-
-            ApplicationUser applicationUser = _context.ApplicationUsers.Find(runnerId);
-
-            if (applicationUser == null)
-            {
-                return RedirectToAction("Index");
-            }
-
-            var dbQueries = (from wi in _context.WorkoutInformation
-                             join wt in _context.WorkoutTypes
-                             on wi.WorkoutTypeId equals wt.Id
-                             join p in _context.Practices
-                             on wi.PracticeId equals p.Id
-                             where wi.RunnerId == runnerId && wi.DataWasLogged
-                             select new 
-                             {
-                                wi.Distance,
-                                wi.Hours,
-                                wi.Minutes,
-                                wi.Seconds,
-                                wt.WorkoutName,
-                                p.PracticeStartTimeAndDate
-                             });
-
-            if (dbQueries == null || dbQueries.Count() < 1)
-            {
-                return RedirectToAction("Index");
-            }
-
-            SelectedRunnerViewModel selectedRunnerViewModel = new SelectedRunnerViewModel { RunnerName = $"{applicationUser.FirstName} {applicationUser.LastName}" };
-            
-            foreach (var dbQuery in dbQueries)
-            {
-                SelectedRunnerViewModelHelper selectedRunnerViewModelHelper = new SelectedRunnerViewModelHelper
-                {
-                    Distance = dbQuery.Distance,
-                    PracticeDate = DateOnly.FromDateTime(dbQuery.PracticeStartTimeAndDate),
-                    WorkoutName = dbQuery.WorkoutName,
-                    LoggedWorkoutTime = new TimeSpan(dbQuery.Hours,dbQuery.Minutes,dbQuery.Seconds),
-                    Pace = StaticPaceCalculator.CalculatePace(dbQuery.Hours, dbQuery.Minutes, dbQuery.Seconds, dbQuery.Distance),
-                };
-
-                selectedRunnerViewModelHelper.PaceString = $"{selectedRunnerViewModelHelper.Pace.Item1}:";
-                selectedRunnerViewModelHelper.PaceString += selectedRunnerViewModelHelper.Pace.Item2 < 10 ? $"0{selectedRunnerViewModelHelper.Pace.Item2}" : $"{selectedRunnerViewModelHelper.Pace.Item2}";
-                selectedRunnerViewModel.SelectedRunnerViewModelHelpers?.Add(selectedRunnerViewModelHelper);
-            }
-             
-            return View(selectedRunnerViewModel);
-        }
+        
     }
 }
